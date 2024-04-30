655f5858cdca2e6e4145cfbc7e37e1d8f04e2dac
dwivedia988 <153066681+dwivedia988@users.noreply.github.com>
2024-05-01 00:22:45 +0530

    fix bug

--- app.js
+++ app.js
@@ -10,47 +10,45 @@ const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
 
 nextbutton.addEventListener("click", checkBillInput);
 
-function checkBillInput(){
-    var inputValue = billAmount.value;
-    if (inputValue.trim() !== '') {
-        fullPage.style.display = "block";
-      } else {
-        alert('Please enter the bill amount.');
-      }
- }
-
-
+function checkBillInput() {
+  var inputValue = billAmount.value;
+  if (inputValue.trim() !== "") {
+    fullPage.style.display = "block";
+  } else {
+    alert("Please enter the bill amount.");
+  }
+}
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
-    hideMessage();
-    if (billAmount.value > 0) {
-        if (cashGiven.value >= billAmount.value) {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
-        } else {
-            showMessage("Do you wanna wash plates?");
-        }
-
+  hideMessage();
+  console.log(billAmount.value);
+  if (billAmount.value > 0) {
+    console.log(cashGiven.value);
+    console.log(cashGiven >= billAmount);
+    if (cashGiven >= billAmount) {
+      let amountToBeReturned = cashGiven.value - billAmount.value;
+      calculateChange(amountToBeReturned);
     } else {
-        showMessage("Invalid Bill Amount");
+      showMessage("Do you wanna wash plates?");
     }
-
-} );
-
+  } else {
+    showMessage("Invalid Bill Amount");
+  }
+});
 
 function calculateChange(amountToBeReturned) {
-    for(let i = 0; i < availableNotes.length; i++){
-        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-        amountToBeReturned = Math.trunc(amountToBeReturned % availableNotes[i]);
-        noOfNotes[i].innerText = numberOfNotes;
-    }
+  for (let i = 0; i < availableNotes.length; i++) {
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    amountToBeReturned = Math.trunc(amountToBeReturned % availableNotes[i]);
+    noOfNotes[i].innerText = numberOfNotes;
+  }
 }
 
 function hideMessage() {
-    message.style.display = "none";
+  message.style.display = "none";
 }
 
 function showMessage(msg) {
-    message.style.display = "block";
-    message.innerText = msg;
+  message.style.display = "block";
+  message.innerText = msg;
 }
